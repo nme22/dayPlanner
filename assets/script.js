459ce50a2c5a1db869ddad16dacf62f05b663fcd
Navid Ebrahimi <navidmebrahimi22@gmail.com>
2021-02-13 19:27:44 -0500

    Got current date and time to appear

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,15 @@
 console.log("my javascript hit")
+
+
+
+var time = moment().format("dddd, MMMM Do YYYY, h:mm a");
+$("#currentDay").text(time);
+
+// timeblock's color changes based on the time vs the time of the block.
+if ()
+// If the time block is in the past change the color to grey
+// If the time block is the current hour then change the color of the block to green
+// If the time block is in the future, change the color of the block to red
+
+// When you click into the time block you can enter an event
+// When we click the save button that the text in the block is saved to local storage.
