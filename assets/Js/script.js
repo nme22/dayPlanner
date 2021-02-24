61728d86b3dce5e916bcc191f7dcaca5aab4bc76
Navid Ebahimi <navidmebrahimi22@gmail.com>
2021-02-24 17:02:19 -0500

    testing some things

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -8,11 +8,6 @@ $(document).ready(function () {
   $('#currentDay').text(date);
 
 
-  //Set styling so that business hours that have passed are greyed out;  if / else for time component 
-  //Set styling so that the current time/hour is a red color;
-  //Set styling so that hours that have not yet occured are in green 
-
-
   function currentTime() {
     var current = moment().hours()
     var time = $('.row');
@@ -24,19 +19,23 @@ $(document).ready(function () {
 
     //  console.log(hour);
 
+    //Set styling so that business hours that have passed are greyed out;  if / else for time component 
+    //Set styling so that the current time/hour is a red color;
+    //Set styling so that hours that have not yet occured are in green 
+
       if (hour === current) {
-        $(this).children('.col-10').attr('class', 'present col-10 description')
-    //    console.log(('present' + hour + current))
+        $(this).children('.col-10').attr('class', 'present col-10 textInput')
+       console.log(('present' + hour + current))
       } else if (current > hour) {
-        $(this).children('.col-10').attr('class', 'past col-10 description')
-    //    console.log(('past' + hour + current))
+        $(this).children('.col-10').attr('class', 'past col-10 textInput')
+       console.log(('past' + hour + current))
       } else {
-        $(this).children('.col-10').attr('class', 'future col-10 description')
-    //    console.log(('future' + hour + current))
+        $(this).children('.col-10').attr('class', 'future col-10 textInput')
+       console.log(('future' + hour + current))
       }
     })
   }
-  currentTime()
+  currentTime();
 
   //Set up local storage to store changes in events by timeblock
 
@@ -57,7 +56,6 @@ $(document).ready(function () {
   $('#hour-15 textarea').val(localStorage.getItem('hour-15').replace(/['"]+/g, ''));
   $('#hour-16 textarea').val(localStorage.getItem('hour-16').replace(/['"]+/g, ''));
   $('#hour-17 textarea').val(localStorage.getItem('hour-17')).replace(/['"]+/g, '');
-//  console.log(parent);
 
   
 
@@ -65,6 +63,5 @@ $(document).ready(function () {
     $('selector').css('cursor', 'i')
   })
   
-
 
 });
