e4bc525f95dc719179409e56cb4d7181944ced00
Navid Ebahimi <navidmebrahimi22@gmail.com>
2021-02-24 14:38:57 -0500

    updated file structure and redid some js

---
+++ assets/Js/script.js
@@ -0,0 +1,9 @@
+var button = $('.saveBtn')
+
+//Add current date at the top of the planner 
+
+$(document).ready(function () {
+  var date = moment().format('dddd MMMM Do, hh:mm a');
+
+  $('#currentDay').text(date);
+});
